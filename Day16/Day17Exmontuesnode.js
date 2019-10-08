417545fb91cfeebae8d136717b6254b7bc2922ab
Jeremy <bluej3710@gmail.com>
2019-10-08 14:54:36 -0400

    Day16/17nodeexport

---
+++ Day16/Day17Exmontuesnode.js
@@ -0,0 +1,4 @@
+const required = require("./index2.js")
+//Can be called anything instead of "required"
+console.log(required.add(2,2));
+console.log(required sub(5,2));
---
+++ Day16/index2.js
@@ -0,0 +1,12 @@
+module.exports = exported;
+//or you can write just the function by itself without the obj
+const exported =
+{ 
+    add : function (x,y) {
+        return x+y;
+    },
+
+    sub: function (x,y) {
+        return (x-y); 
+    }
+}
